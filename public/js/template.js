926c080f5bb7f54e2aa74242721e638d91c44af3
Fariez <cihilxd46@gmail.com>
2025-11-28 10:06:19 +0700

    Update template.js

--- public/js/template.js
+++ public/js/template.js
@@ -1,14 +1,21 @@
-// assets/js/template.js
 async function loadTemplate(id, file) {
   const container = document.getElementById(id);
-  if (container) {
-    const response = await fetch(file);
-    const html = await response.text();
-    container.innerHTML = html;
+  if (!container) return;
+
+  const cached = localStorage.getItem("template_" + file);
+  if (cached) {
+    container.innerHTML = cached;
+    return;
   }
+
+  const response = await fetch(file);
+  const html = await response.text();
+
+  container.innerHTML = html;
+
+  localStorage.setItem("template_" + file, html);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadTemplate("navbar-container", "./templates/navbar.html");
- 
+  loadTemplate("navbar-container", "/templates/navbar.html");
 });
